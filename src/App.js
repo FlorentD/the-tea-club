f185c2e542f9d951b03e50702e63e1dde0ee92cf
Florent DUVEAU <florent.duveau@kisskissbankbank.com>
2020-04-22 11:46:20 +0200

    create landing page

--- src/App.js
+++ src/App.js
@@ -1,43 +1,19 @@
-/* global fetch */
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Box, Container, Grid } from "@material-ui/core";
+import Logo from "./svg/Logo";
 
 const App = () => {
-  const [message, setMessage] = useState('...loading')
-
-  useEffect(() => {
-    async function fetchData () {
-      try {
-        let data = await (await fetch('/api')).json()
-        setMessage(data.message)
-      } catch (err) {
-        setMessage(err.message)
-      }
-    }
-    fetchData()
-  })
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{message}</p>
-        <p>Change me!</p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Grid container justify="center">
+        <Grid item xs={6}>
+          <Box mt={4}>
+            <Logo />
+          </Box>
+        </Grid>
+      </Grid>
+    </Container>
   );
-}
+};
 
 export default App;
