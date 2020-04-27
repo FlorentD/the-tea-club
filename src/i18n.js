b10c137e79bedd07413c3c5f9897ac63afa266fe
Florent DUVEAU <florent.duveau@kisskissbankbank.com>
2020-04-27 16:28:08 +0200

    add translation system

--- src/i18n.js
+++ src/i18n.js
@@ -1,27 +1,37 @@
 import i18n from "i18next";
+import flow from "lodash/fp/flow";
+import mapValues from "lodash/fp/mapValues";
+import reduce from "lodash/fp/reduce";
+import translations from "./translations";
 import { initReactI18next } from "react-i18next";
+
+const explode = (data, index = 0) => {
+  const firstKey = Object.keys(data)[0];
+  const splitKeys = firstKey.split(".");
+  return {
+    [splitKeys[index]]:
+      index < splitKeys.length - 1 ? explode(data, index + 1) : data[firstKey],
+  };
+};
+
+const resources = (languages) => {
+  return languages.reduce((acc, lang) => {
+    acc[lang] = {
+      translation: flow(
+        mapValues(lang),
+        reduce.convert({ cap: false })((acc, value, key) => {
+          return { ...acc, ...explode({ [key]: value }) };
+        }, {})
+      )(translations),
+    };
+    return acc;
+  }, {});
+};
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
-    resources: {
-      en: {
-        translation: {
-          welcome: "With",
-          map: {
-            title: "Where to find me",
-          },
-        },
-      },
-      fr: {
-        translation: {
-          welcome: "Avec",
-          map: {
-            title: "Ou me trouver",
-          },
-        },
-      },
-    },
+    resources: resources(["fr", "en"]),
     lng: "fr",
     fallbackLng: "en",
   })
---
+++ src/translations.js
@@ -0,0 +1,10 @@
+export default {
+  welcome: {
+    fr: "Avec",
+    en: "With",
+  },
+  "map.title": {
+    fr: "Ou me trouver",
+    en: "Where to find me",
+  },
+};
