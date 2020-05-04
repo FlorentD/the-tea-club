e996077f8b50ecdbae1108caec90fcdc905ed1e7
Florent DUVEAU <florent.duveau@kisskissbankbank.com>
2020-05-04 10:29:06 +0200

    add TextBox and TextContainer to ParralaxImage

--- src/components/ParallaxImage.js
+++ src/components/ParallaxImage.js
@@ -1,9 +1,59 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Background1 from "../svg/Background1";
-import { Container } from "@material-ui/core";
+import { Container, Box, makeStyles, Grid } from "@material-ui/core";
 import BackgroundBottom from "../svg/BackgroundBottom";
 import { ParallaxBanner } from "react-scroll-parallax";
+import ShowOnView from "./ShowOnView";
+
+const useStyleBox = makeStyles((theme) => ({
+  root: {
+    backgroundColor: "rgba(255,255,255,0.9)",
+    padding: "10px 15px",
+    [theme.breakpoints.up("md")]: {
+      padding: "30px 50px",
+    },
+  },
+}));
+
+const TextBox = ({ children, ...props }) => {
+  const styleBox = useStyleBox();
+  return (
+    <ShowOnView>
+      <Box {...props} className={styleBox.root}>
+        {children}
+      </Box>
+    </ShowOnView>
+  );
+};
+
+TextBox.propTypes = {
+  dark: PropTypes.bool,
+  light: PropTypes.bool,
+};
+
+TextBox.DefautProps = {
+  dark: false,
+  light: true,
+};
+
+const useStyleContainer = makeStyles({
+  root: {
+    position: "relative",
+    zIndex: 100,
+  },
+});
+
+const TextContainer = ({ children, ...props }) => {
+  const style = useStyleContainer();
+  return (
+    <Grid container className={style.root}>
+      <Grid item {...props}>
+        {children}
+      </Grid>
+    </Grid>
+  );
+};
 
 const ParallaxImage = ({ src, amount, children, height, soft }) => {
   return (
@@ -34,5 +84,8 @@ ParallaxImage.defaultProps = {
   height: "60vh",
   soft: true,
 };
+
+ParallaxImage.Box = TextBox;
+ParallaxImage.Container = TextContainer;
 
 export default ParallaxImage;
--- src/me/childhood.js
+++ src/me/childhood.js
@@ -1,51 +1,24 @@
 import React from "react";
-import { Grid, Box, Typography, makeStyles } from "@material-ui/core";
-import { styled } from "@material-ui/core";
-import ShowOnView from "../components/ShowOnView";
+import { Typography } from "@material-ui/core";
 import ParallaxImage from "../components/ParallaxImage";
-
-const useStyleBox = makeStyles((theme) => ({
-  root: {
-    backgroundColor: "rgba(255,255,255,0.9)",
-    padding: "10px 15px",
-    [theme.breakpoints.up("md")]: {
-      padding: "30px 50px",
-    },
-  },
-}));
-
-const Text = styled(({ className }) => {
-  const styleBox = useStyleBox();
-  return (
-    <Grid container className={className}>
-      <Grid item xs={12} md={8}>
-        <ShowOnView>
-          <Box
-            mt={{ xs: 4, md: 10 }}
-            ml={{ xs: 0, md: 5 }}
-            m={{ xs: 1, md: 10 }}
-            className={styleBox.root}
-          >
-            <Typography variant="h5">
-              Je suis née à <strong>Metz</strong> en Lorraine, dans un bain
-              linguistique plutôt allemand et en même temps très curieuse de
-              découvrir cette “langue des chansons” apprise par les copines des
-              autres classes : <strong>l’ANGLAIS</strong>.
-            </Typography>
-          </Box>
-        </ShowOnView>
-      </Grid>
-    </Grid>
-  );
-})({
-  position: "relative",
-  zIndex: 100,
-});
 
 const Childhood = () => {
   return (
     <ParallaxImage src="/static/apples.jpg">
-      <Text />
+      <ParallaxImage.Container xs={12} md={8}>
+        <ParallaxImage.Box
+          mt={{ xs: 4, md: 10 }}
+          ml={{ xs: 0, md: 5 }}
+          m={{ xs: 1, md: 10 }}
+        >
+          <Typography variant="h5">
+            Je suis née à <strong>Metz</strong> en Lorraine, dans un bain
+            linguistique plutôt allemand et en même temps très curieuse de
+            découvrir cette “langue des chansons” apprise par les copines des
+            autres classes : <strong>l’ANGLAIS</strong>.
+          </Typography>
+        </ParallaxImage.Box>
+      </ParallaxImage.Container>
     </ParallaxImage>
   );
 };
