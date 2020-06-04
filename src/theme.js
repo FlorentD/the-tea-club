27c38b8e9bf741fee53f875039af2c2e1ea66663
Florent DUVEAU <florent.duveau@kisskissbankbank.com>
2020-06-04 23:11:38 +0200

    add Itim police

--- src/theme.js
+++ src/theme.js
@@ -2,7 +2,7 @@ import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
 
 let theme = createMuiTheme({
   typography: {
-    fontFamily: ["Kalam", "Roboto", "Arial"].join(","),
+    fontFamily: ["Itim", "Roboto", "Arial"].join(","),
     h2: {
       fontFamily: ["Marck Script", "Roboto", "Arial"].join(","),
     },
